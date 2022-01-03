f147646c9d6105eb3da99cab57c749dfb0338407
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-01-03 23:00:57 +0530

    Adding Function/AddeventListner to choose Salary Range

---
+++ Employee-Pyroll-App(HTML-CSS)/JavaScript-all-here/employee-payroll.js
@@ -0,0 +1,7 @@
+/*Adding Eventlistner for Salary Range*/
+const salary = document.querySelector('#salary');
+const salaryOutput = document.querySelector('.salary-output');
+salaryOutput.textContent = salary.value;
+salary.addEventListener('input',function(){
+    salaryOutput.textContent = salary.value;
+});
