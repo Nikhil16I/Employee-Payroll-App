1522e80e2965b11f13705b184ae5dcd465831a23
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-01-06 01:15:33 +0530

    Validating Name and Setting Start date as Condition

--- Employee-Pyroll-App(HTML-CSS)/JavaScript-all-here/employee_payroll.js
+++ Employee-Pyroll-App(HTML-CSS)/JavaScript-all-here/employee_payroll.js
@@ -1,7 +1,31 @@
-/*Adding Eventlistner for Salary Range*/
-const salary = document.querySelector('#salary');
-const salaryOutput = document.querySelector('.salary-output');
-salaryOutput.textContent = salary.value;
-salary.addEventListener('input',function(){
+function Salary() {
+    /*Adding Eventlistner for Salary Range*/
+    const salary = document.querySelector('#salary');
+    const salaryOutput = document.querySelector('.salary-output');
     salaryOutput.textContent = salary.value;
-});
+    salary.addEventListener('input', function () {
+        salaryOutput.textContent = salary.value;
+    })
+}
+/*Adding event listener for Name while validating name*/
+function Namevalidaiton() {
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.name-error');
+    name.addEventListener('input', function () {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$')
+        if (nameRegex.test(name.value)) {
+            nameError.textContent = " ";
+        }
+        else {
+            nameError.textContent = "Name is Invalid,Should start with Capital Letters";
+        }
+    })
+}
+function Startdate() {
+
+}
+window.addEventListener('DOMContentLoaded', (Event) => {
+    Salary();
+    Namevalidaiton();
+    Startdate();
+})
--- Employee-Pyroll-App(HTML-CSS)/JavaScript-all-here/employee_payrollData.js
+++ Employee-Pyroll-App(HTML-CSS)/JavaScript-all-here/employee_payrollData.js
@@ -1,38 +1,41 @@
 class employeepayrollData {
-    
-    get id() { return this._id}
-    set id(id) {  
+
+    get id() { return this._id }
+    set id(id) {
         this._id = id
     }
 
-    get name() { return this._name}
+    get name() { return this._name }
     set name(name) {
         this._name = name;
     }
 
-    get profilePic() {return this._profilePic}
+    get profilePic() { return this._profilePic }
     set profilePic(profilePic) {
         this._profilePic = profilePic
     }
 
-    get gender() {return this._gender}
+    get gender() { return this._gender }
     set gender(gender) {
         this._gender = gender
     }
 
-    get department() { return this._department}
+    get department() { return this._department }
     set department(department) {
         this._department = department
     }
-    
-    get salary() {return this._salary}
+
+    get salary() { return this._salary }
     set salary(salary) {
         this._salary = salary
     }
 
-    get note() {return this._note}
+    get note() { return this._note }
     set note(note) {
         this._note = note
     }
-    
+    get startdate() { return this._startdate }
+    set startdate(startdate) {
+        this._startdate = startdate
+    }
 }
