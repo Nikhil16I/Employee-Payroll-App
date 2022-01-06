51415c7fa685800f7070baf37a7ba1ce44420091
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-01-07 01:28:55 +0530

    Refactoring

--- Employee-Pyroll-App(HTML-CSS)/JavaScript-all-here/employee_payroll.js
+++ Employee-Pyroll-App(HTML-CSS)/JavaScript-all-here/employee_payroll.js
@@ -36,6 +36,48 @@ function Startdate() {
             }
         });
     }
+    const save = () => {
+        try {
+            let employeePayrollData = createEmployeePayroll();
+        } catch (e) {
+            return;
+        }
+    }
+    
+    const createEmployeePayroll = () => {
+        let employeePayrollData = new employee_payrollData();
+        try {
+            employeePayrollData.name = getInputValueById('#name');
+        } catch (e) {
+            setTextValue('.text-error', e);
+            throw e;
+        }
+    
+        employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+        employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+        employeePayrollData.department = getSelectedValues('[name=department]');
+        employeePayrollData.salary = getInputValueById('#salary');
+        employeePayrollData.note = getInputValueById('#notes');
+        let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+        employeePayrollData.start_date = Date.parse(date);
+        console.log(employeePayrollData.toString());
+        return employeePayrollData;
+    }
+    
+    const getSelectedValues = (propertyValue) => {
+        let allItems = document.querySelectorAll(propertyValue);
+        let selItems = [];
+        allItems.forEach(item => {
+            if (item.checked)
+                selItems.push(item.value);
+        });
+        return selItems;
+    }
+    
+    const getInputValueById = (id) => {
+        let value = document.querySelector(id).value;
+        return value;
+    }
 
 window.addEventListener('DOMContentLoaded', (Event) => {
     Salary();
